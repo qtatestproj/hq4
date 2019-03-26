406dd3dd90ec0793c6a1e67effe9d5fb68d6d65b
qtateam <44162446+qtateam@users.noreply.github.com>
2019-03-26 10:48:37 +0800

    add settings.py

---
+++ settings.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+'''测试项目配置文件
+'''
+#2019/03/26 QTAF自动生成
+
+import os
+
+PROJECT_NAME = "hq4"
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+PROJECT_MODE = "standard"
+INSTALLED_APPS = []
+
+DRUN_TASK_ENTRY = 'task'
+try:
+    from drun_settings import *
+except ImportError:
+    pass
